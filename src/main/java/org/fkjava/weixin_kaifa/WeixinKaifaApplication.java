2d3ac6044bcc487d077ddb64a5c5d79afda53705
Administrator <Administrator@PC-20170930RVDD>
2019-03-22 15:04:31 +0800

    初次提交Spring Boot的项目模板

---
+++ src/main/java/org/fkjava/weixin_kaifa/WeixinKaifaApplication.java
@@ -0,0 +1,13 @@
+package org.fkjava.weixin_kaifa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeixinKaifaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeixinKaifaApplication.class, args);
+	}
+
+}
